a27f733609a6583b3acec8c8b74d9281f0b0fa42
Joel López Aguilera <joellopezaguilera@gmail.com>
2026-02-03 11:15:24 +0100

    a

--- src/main/java/com/institut/cloudapi/CloudapiApplication.java
+++ src/main/java/com/institut/cloudapi/CloudapiApplication.java
@@ -2,12 +2,17 @@ package com.institut.cloudapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.StandardEnvironment;
 
 @SpringBootApplication
 public class CloudapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudapiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(CloudapiApplication.class);
+        ConfigurableEnvironment env = new StandardEnvironment();
+        System.out.println("DB_URL=" + env.getProperty("DB_URL"));
+        app.run(args);
+    }
 
 }
--- src/test/java/com/institut/cloudapi/CloudapiApplicationTests.java
+++ src/test/java/com/institut/cloudapi/CloudapiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gabriela.cloudapi;
+package com.institut.cloudapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
